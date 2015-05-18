73b2390354cd43b158da0581cd28ef20342b6c77
Denni Adam <dennila2@ya.ru>
2015-05-18 18:17:26 +0300

    v 14 return message

--- src/android/Alert.java
+++ src/android/Alert.java
@@ -57,7 +57,7 @@ public class Alert extends CordovaPlugin {
         .setNeutralButton(buttonLabel, new AlertDialog.OnClickListener() {
             public void onClick(DialogInterface dialogInterface, int which) {
                 dialogInterface.dismiss();
-                callbackContext.success("Hello, WORD!");
+                callbackContext.success(message);
                 callbackContext.error("case error!");
             }
         })
